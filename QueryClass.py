a3750f0473befa8b403469cd2357156b8152a58b
Dasister <maxsyrman@yandex.ru>
2018-06-05 12:00:49 +0300

    Some code changes.

--- QueryClass.py
+++ QueryClass.py
@@ -2,6 +2,11 @@
 # Support: Source Servers, GoldSrc servers, The Ship Servers
 # ToDo: Bugfixes
 
+import socket
+import struct
+import sys
+import time
+
 __author__ = 'Dasister'
 __site__ = 'http://21games.ru'
 __description__ = 'Simple Query Class for VALVe servers'
@@ -13,25 +18,19 @@ A2S_RULES = b'\xFF\xFF\xFF\xFF\x56'
 S2A_INFO_SOURCE = chr(0x49)
 S2A_INFO_GOLDSRC = chr(0x6D)
 
-import socket
-import time
-import struct
-import sys
-
 
 class SourceQuery(object):
-
     is_third = False
 
     def __init__(self, addr, port=27015, timeout=5.0):
         self.ip, self.port, self.timeout = socket.gethostbyname(addr), port, timeout
-        self.sock = False
-        self.challenge = False
+        self.sock = None
+        self.challenge = None
         if sys.version_info >= (3, 0):
             self.is_third = True
 
     def disconnect(self):
-        if self.sock:
+        if self.sock is not None:
             self.sock.close()
             self.sock = False
 
@@ -41,11 +40,12 @@ class SourceQuery(object):
         self.sock.settimeout(self.timeout)
         self.sock.connect((self.ip, self.port))
 
-    def getPing(self):
-        return self.getInfo()['Ping']
+    def get_ping(self):
+        return self.get_info()['Ping']
 
-    def getInfo(self):
-        self.connect()
+    def get_info(self):
+        if self.sock is None:
+            self.connect()
         self.sock.send(A2S_INFO)
         before = time.time()
         try:
@@ -58,19 +58,19 @@ class SourceQuery(object):
 
         result = {}
 
-        header, data = self.getByte(data)
+        header, data = self.__get_byte(data)
         result['Ping'] = int((after - before) * 1000)
         if chr(header) == S2A_INFO_SOURCE:
-            result['Protocol'], data = self.getByte(data)
-            result['Hostname'], data = self.getString(data)
-            result['Map'], data = self.getString(data)
-            result['GameDir'], data = self.getString(data)
-            result['GameDesc'], data = self.getString(data)
-            result['AppID'], data = self.getShort(data)
-            result['Players'], data = self.getByte(data)
-            result['MaxPlayers'], data = self.getByte(data)
-            result['Bots'], data = self.getByte(data)
-            dedicated, data = self.getByte(data)
+            result['Protocol'], data = self.__get_byte(data)
+            result['Hostname'], data = self.__get_string(data)
+            result['Map'], data = self.__get_string(data)
+            result['GameDir'], data = self.__get_string(data)
+            result['GameDesc'], data = self.__get_string(data)
+            result['AppID'], data = self.__get_short(data)
+            result['Players'], data = self.__get_byte(data)
+            result['MaxPlayers'], data = self.__get_byte(data)
+            result['Bots'], data = self.__get_byte(data)
+            dedicated, data = self.__get_byte(data)
             if chr(dedicated) == 'd':
                 result['Dedicated'] = 'Dedicated'
             elif dedicated == 'l':
@@ -78,73 +78,73 @@ class SourceQuery(object):
             else:
                 result['Dedicated'] = 'SourceTV'
 
-            os, data = self.getByte(data)
+            os, data = self.__get_byte(data)
             if chr(os) == 'w':
                 result['OS'] = 'Windows'
             elif chr(os) in ('m', 'o'):
                 result['OS'] = 'Mac'
             else:
                 result['OS'] = 'Linux'
-            result['Password'], data = self.getByte(data)
-            result['Secure'], data = self.getByte(data)
+            result['Password'], data = self.__get_byte(data)
+            result['Secure'], data = self.__get_byte(data)
             if result['AppID'] == 2400:  # The Ship server
-                result['GameMode'], data = self.getByte(data)
-                result['WitnessCount'], data = self.getByte(data)
-                result['WitnessTime'], data = self.getByte(data)
-            result['Version'], data = self.getString(data)
-            edf, data = self.getByte(data)
+                result['GameMode'], data = self.__get_byte(data)
+                result['WitnessCount'], data = self.__get_byte(data)
+                result['WitnessTime'], data = self.__get_byte(data)
+            result['Version'], data = self.__get_string(data)
+            edf, data = self.__get_byte(data)
             try:
                 if edf & 0x80:
-                    result['GamePort'], data = self.getShort(data)
+                    result['GamePort'], data = self.__get_short(data)
                 if edf & 0x10:
-                    result['SteamID'], data = self.getLongLong(data)
+                    result['SteamID'], data = self.__get_long_long(data)
                 if edf & 0x40:
-                    result['SpecPort'], data = self.getShort(data)
-                    result['SpecName'], data = self.getString(data)
+                    result['SpecPort'], data = self.__get_short(data)
+                    result['SpecName'], data = self.__get_string(data)
                 if edf & 0x10:
-                    result['Tags'], data = self.getString(data)
+                    result['Tags'], data = self.__get_string(data)
             except:
                 pass
         elif chr(header) == S2A_INFO_GOLDSRC:
-            result['GameIP'], data = self.getString(data)
-            result['Hostname'], data = self.getString(data)
-            result['Map'], data = self.getString(data)
-            result['GameDir'], data = self.getString(data)
-            result['GameDesc'], data = self.getString(data)
-            result['Players'], data = self.getByte(data)
-            result['MaxPlayers'], data = self.getByte(data)
-            result['Version'], data = self.getByte(data)
-            dedicated, data = self.getByte(data)
+            result['GameIP'], data = self.__get_string(data)
+            result['Hostname'], data = self.__get_string(data)
+            result['Map'], data = self.__get_string(data)
+            result['GameDir'], data = self.__get_string(data)
+            result['GameDesc'], data = self.__get_string(data)
+            result['Players'], data = self.__get_byte(data)
+            result['MaxPlayers'], data = self.__get_byte(data)
+            result['Version'], data = self.__get_byte(data)
+            dedicated, data = self.__get_byte(data)
             if chr(dedicated) == 'd':
                 result['Dedicated'] = 'Dedicated'
             elif dedicated == 'l':
                 result['Dedicated'] = 'Listen'
             else:
                 result['Dedicated'] = 'HLTV'
-            os, data = self.getByte(data)
+            os, data = self.__get_byte(data)
             if chr(os) == 'w':
                 result['OS'] = 'Windows'
             else:
                 result['OS'] = 'Linux'
-            result['Password'], data = self.getByte(data)
-            result['IsMod'], data = self.getByte(data)
+            result['Password'], data = self.__get_byte(data)
+            result['IsMod'], data = self.__get_byte(data)
             if result['IsMod']:
-                result['URLInfo'], data = self.getString(data)
-                result['URLDownload'], data = self.getString(data)
-                data = self.getByte(data)[1]  # NULL-Byte
-                result['ModVersion'], data = self.getLong(data)
-                result['ModSize'], data = self.getLong(data)
-                result['ServerOnly'], data = self.getByte(data)
-                result['ClientDLL'], data = self.getByte(data)
-            result['Secure'], data = self.getByte(data)
-            result['Bots'], data = self.getByte(data)
+                result['URLInfo'], data = self.__get_string(data)
+                result['URLDownload'], data = self.__get_string(data)
+                data = self.__get_byte(data)[1]  # NULL-Byte
+                result['ModVersion'], data = self.__get_long(data)
+                result['ModSize'], data = self.__get_long(data)
+                result['ServerOnly'], data = self.__get_byte(data)
+                result['ClientDLL'], data = self.__get_byte(data)
+            result['Secure'], data = self.__get_byte(data)
+            result['Bots'], data = self.__get_byte(data)
 
         return result
 
-# <------------------getInfo() End -------------------------->
+    # <------------------getInfo() End -------------------------->
 
-    def getChallenge(self):
-        if not self.sock:
+    def get_challenge(self):
+        if self.sock is None:
             self.connect()
         self.sock.send(A2S_PLAYERS + b'0xFFFFFFFF')
         try:
@@ -155,13 +155,14 @@ class SourceQuery(object):
         self.challenge = data[5:]
 
         return True
-# <-------------------getChallenge() End --------------------->
 
-    def getPlayers(self):
-        if not self.sock:
+    # <-------------------getChallenge() End --------------------->
+
+    def get_players(self):
+        if self.sock is None:
             self.connect()
-        if not self.challenge:
-            self.getChallenge()
+        if self.challenge is None:
+            self.get_challenge()
 
         self.sock.send(A2S_PLAYERS + self.challenge)
         try:
@@ -171,17 +172,17 @@ class SourceQuery(object):
 
         data = data[4:]
 
-        header, data = self.getByte(data)
-        num, data = self.getByte(data)
+        header, data = self.__get_byte(data)
+        num, data = self.__get_byte(data)
         result = []
         try:
             for i in range(num):
                 player = {}
-                data = self.getByte(data)[1]
+                data = self.__get_byte(data)[1]
                 player['id'] = i + 1  # ID of All players is 0
-                player['Name'], data = self.getString(data)
-                player['Frags'], data = self.getLong(data)
-                player['Time'], data = self.getFloat(data)
+                player['Name'], data = self.__get_string(data)
+                player['Frags'], data = self.__get_long(data)
+                player['Time'], data = self.__get_float(data)
                 ftime = time.gmtime(int(player['Time']))
                 player['FTime'] = ftime
                 player['PrettyTime'] = time.strftime('%H:%M:%S', ftime)
@@ -192,13 +193,13 @@ class SourceQuery(object):
 
         return result
 
-# <-------------------getPlayers() End ----------------------->
+    # <-------------------getPlayers() End ----------------------->
 
-    def getRules(self):
-        if not self.sock:
+    def get_rules(self):
+        if self.sock is None:
             self.connect()
-        if not self.challenge:
-            self.getChallenge()
+        if self.challenge is None:
+            self.get_challenge()
 
         self.sock.send(A2S_RULES + self.challenge)
         try:
@@ -218,43 +219,43 @@ class SourceQuery(object):
             return False
         data = data[4:]
 
-        header, data = self.getByte(data)
-        num, data = self.getShort(data)
+        header, data = self.__get_byte(data)
+        num, data = self.__get_short(data)
         result = {}
 
         # Server sends incomplete packets. Ignore "NumPackets" value.
         while 1:
             try:
-                ruleName, data = self.getString(data)
-                ruleValue, data = self.getString(data)
-                if ruleValue:
-                    result[ruleValue] = ruleName
+                rule_name, data = self.__get_string(data)
+                rule_value, data = self.__get_string(data)
+                if rule_value:
+                    result[rule_value] = rule_name
             except:
                 break
 
         return result
 
-# <-------------------getRules() End ------------------------->
+    # <-------------------getRules() End ------------------------->
 
-    def getByte(self, data):
+    def __get_byte(self, data):
         if self.is_third:
             return data[0], data[1:]
         else:
             return ord(data[0]), data[1:]
 
-    def getShort(self, data):
+    def __get_short(self, data):
         return struct.unpack('<h', data[0:2])[0], data[2:]
 
-    def getLong(self, data):
+    def __get_long(self, data):
         return struct.unpack('<l', data[0:4])[0], data[4:]
 
-    def getLongLong(self, data):
+    def __get_long_long(self, data):
         return struct.unpack('<Q', data[0:8])[0], data[8:]
 
-    def getFloat(self, data):
+    def __get_float(self, data):
         return struct.unpack('<f', data[0:4])[0], data[4:]
 
-    def getString(self, data):
+    def __get_string(self, data):
         s = ""
         i = 0
         if not self.is_third:
@@ -271,7 +272,7 @@ class SourceQuery(object):
 # Just for testing
 if __name__ == '__main__':
     query = SourceQuery(sys.argv[1], 27015)
-    res = query.getInfo()
+    res = query.get_info()
     print(res['Hostname'])
     print(res['Map'])
     print(res['GameDir'])
@@ -280,15 +281,17 @@ if __name__ == '__main__':
     print(res['Tags'])
     print(res['Ping'])
 
-    players = query.getPlayers()
+    players = query.get_players()
 
     for player in players:
         print("{id:<2} {Name:<35} {Frags:<5} {PrettyTime}".format(**player))
 
-    rules = query.getRules()
+    rules = query.get_rules()
 
-    print "\n{0:d} Rules".format(len(rules))
-    print "------------------------------------"
+    print
+    "\n{0:d} Rules".format(len(rules))
+    print
+    "------------------------------------"
     for rule_name, value in rules.items():
         print("{0:<5} {1}".format(rule_name, value))
     query.disconnect()
